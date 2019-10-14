9cd60f993b4f628034caeb8b6fc23523070577ac
toshnewton <36524417+toshnewton@users.noreply.github.com>
2019-10-14 12:15:13 -0500

    hey now it works from = > to =>

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-
+const hostname = process.env.hostname || '0.0.0.0'
 const port = 3002;
 
-const server = http.createServer((req, res) = > {
-    res.statusCode = 200;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello...\n');
   res.write('Hello...\n');
